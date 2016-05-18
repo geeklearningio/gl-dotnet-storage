fcef0dd8274c1b6fc12e9ae39b947f95813d1fec
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-05-18 17:34:03 +0200

    rc2 support

---
+++ samples/GeekLearning.Storage.BasicSample/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekLearning.Storage.BasicSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        private TemplatesStore templates;
+
+        public ValuesController(TemplatesStore templates)
+        {
+            this.templates = templates;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+
+            return new string[] { await templates.Store.ReadAllText("json.json"), "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ samples/GeekLearning.Storage.BasicSample/TemplatesStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekLearning.Storage.BasicSample
+{
+    public class TemplatesStore : StoreBase
+    {
+        public TemplatesStore(IStorageFactory storageFactory) : base("Templates", storageFactory)
+        {
+        }
+    }
+}
